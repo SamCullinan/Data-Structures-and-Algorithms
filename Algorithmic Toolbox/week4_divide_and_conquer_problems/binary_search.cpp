3c8c519746a8bca12c84fe127d584b27c7ec0849
SamCullinan <novruzov.sb@phystech.edu>
2020-04-15 15:26:23 +0300

    Updated

--- Algorithmic Toolbox/week4_divide_and_conquer_problems/binary_search.cpp
+++ Algorithmic Toolbox/week4_divide_and_conquer_problems/binary_search.cpp
@@ -1,4 +1,3 @@
-
 //
 //  main.cpp
 //  algo
@@ -9,17 +8,12 @@
 
 #include <iostream>
 #include <vector>
-#include <cassert>
-
 using namespace std;
-int binary_search(const vector<int> &num, int value){
-    
+int binary_search(const vector<long long> &num, long long value){
     int left = 0;
     int right = int(num.size());
     while(left<= right){
-        
         int mid = ( left + right )/2;
-        
         if( value == num[mid] )
             return mid;
         else if(value < num[mid])
@@ -35,21 +29,23 @@ int main(int argc, const char * argv[]) {
     
     int n;
     cin >> n;
-    vector<int> num1(n);
+    vector<long long> num1(n);
     
     for(auto& s1: num1)
         cin >> s1;
     
     int m;
     cin >> m;
-    vector<int> num2(m);
+    vector<long long> num2(m);
     
     for(auto& s2: num2)
         cin >> s2;
     
     for (int i = 0; i < m; ++i)
         cout << binary_search(num1, num2[i]) << " ";
-    
+    cout << endl;
     return 0;
 }
+
+
 
